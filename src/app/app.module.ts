b0c5b00de1d92aa517f67289323b27a973a63968
Prashant Ganeriwal <prashant.ganeriwal@algonomy.com>
2024-11-05 16:12:47 +0530

    Working commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,6 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
+  // providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
   providers: [],
   bootstrap: [AppComponent]
 })
